eb71847232c799ca6c6f887f56e129a73fad1d2d
LulledLion:LocalUser <LocalUser@Example.com>
2026-06-20 14:14:21 -0500

    Preset System

---
+++ presets/presets.js
@@ -0,0 +1,19 @@
+var preset;
+var result = `macro EDITOR_ON = ${preset === 0}\n`;
+data = {
+    /* Key: [ file_path, skirmish_maps, TDM_maps ] */
+    example: ["Map_Data.opy", `"workshopExpanse", "workshopIsland"`, `"workshopExpanseNight", "workshopIslandNight"`],
+    1: ["1_LulledLion.opy", `"eichenwalde"`, ``],
+}
+if (preset === 0){
+    result += "macro PRESET_SKIRMISH_MAPS = []\nmacro PRESET_TDM_MAPS = []\n#!define PRESET_MAP_DATA /**/\n";
+}
+else if (preset in data) {
+    result += `macro PRESET_SKIRMISH_MAPS = [${data[preset][1].toLowerCase()}]\n`;
+    result += `macro PRESET_TDM_MAPS = [${data[preset][2].toLowerCase()}]\n`;
+    result += `#!define PRESET_MAP_DATA #!include "presets/${data[preset][0]}"\n`;
+}
+else{
+    throw new RangeError("Preset Not Found!");
+}
+result;
