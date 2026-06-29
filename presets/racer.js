77ee4c43e20577f0b153c00308fbc2aebe300225
LulledLion:LocalUser <LocalUser@Example.com>
2026-06-28 20:36:54 -0500

    adding Ball settings

--- presets/racer.js
+++ presets/racer.js
@@ -1,13 +1,14 @@
 var Hero;
 let Project = {
+        null: [],
         "jetpackCat": ["dsc.gg/jetpack", "TSB2M", "jetpackcat.racing"],
         "shion": [],
         "doomfist": ["dsc.gg/projectmomentum", "NAPGF", "doom.pk"],
         "genji": ["dsc.gg/genjiparkour", "54CRY", "genji.pk"],
-        "kiriko": [],
-        "lucio": ["discord.com/invite/G9QBCDY", "luciosurfleague.com"],
-        "mercy": ["discord.gg/mercyparkour", "mercyparkour.codes"],
+        "lucio": ["discord.com/invite/G9QBCDY", null, "luciosurfleague.com"],
+        "mercy": ["discord.gg/mercyparkour", null, "mercyparkour.codes"],
     }
+let ProjectHero = Project[Hero in Project? Hero : null]
 let Result = `
 enum Hero:
 	JETPACKCAT = Hero.JETPACK_CAT,
@@ -19,8 +20,8 @@ enum Racer:
     String = "${Hero}"
     StringLC = "${Hero.toLowerCase()}"
     StringUC = "${Hero.toUpperCase()}"
-    Discord = "${Project[Hero][0] ?? ''}"
-    ShareCode = "${Project[Hero][1] ?? 'XXXXX'}"
-    Website = "${Project[Hero][2] ?? ''}"
+    Discord = "${ProjectHero[0] ?? ''}"
+    ShareCode = "${ProjectHero[1] ?? 'XXXXX'}"
+    Website = "${ProjectHero[2] ?? ''}"
 `;
 Result;
